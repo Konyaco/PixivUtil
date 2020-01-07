9d108fa4a5589fb0c8dcd7c6b5143418ffd00b69
atwzj233 <atwzj233@gmail.com>
2020-01-07 16:06:30 +0800

    fix bugs of ktor engine

--- ktor/src/main/kotlin/com/atwzj/pixivlib/ktor/KtorEngine.kt
+++ ktor/src/main/kotlin/com/atwzj/pixivlib/ktor/KtorEngine.kt
@@ -10,7 +10,6 @@ import io.ktor.client.engine.http
 import io.ktor.client.request.get
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 
 @KtorExperimentalAPI
@@ -22,31 +21,28 @@ object Ktor : EngineFactory {
 
 @KtorExperimentalAPI
 class KtorEngine : Engine {
-    private val httpClient = HttpClient(Apache) {
-        engine {
-            proxy = ProxyBuilder.http("http://127.0.0.1:1080")
-        }
-    }
-
     /**
      * @throws [PixivException] on failure
      */
     override suspend fun httpGet(url: String): String {
-        return coroutineScope<String> {
-            withContext(Dispatchers.IO) {
-                var response = ""
-                try {
-                    // Try twice
-                    for (i in 1..3) {
-                        response = httpClient.get(url)
-                        break
-                    }
-                } catch (e: Exception) {
-//                    e.printStackTrace()
-                    throw PixivException("Failed to get response", e)
-                }
-                response
+        val httpClient = HttpClient(Apache) {
+            engine {
+                proxy = ProxyBuilder.http("http://127.0.0.1:1080")
             }
         }
+        var response = ""
+        try {
+            // Try twice
+            for (i in 1..3) {
+                response = withContext(Dispatchers.IO) {
+                    httpClient.get<String>(url)
+                }
+                break
+            }
+        } catch (e: Exception) {
+//                    e.printStackTrace()
+            throw PixivException("Failed to get response", e)
+        }
+        return response
     }
 }
